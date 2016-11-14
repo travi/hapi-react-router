3dc90603ac23e2df4588a5bb84d51b59d52f5f52
Matt Travi <programmer@travi.org>
2016-11-14 11:13:48 -0600

    feat(render-route): html requests are now directed through react-router
    routes are matched against react-router routes, components are then mounted, rendered, and passed
    
    along for replying

--- src/route.js
+++ src/route.js
@@ -1,10 +1,10 @@
+import renderThroughReactRouter from './router-wrapper';
+
 export function register(server, options, next) {
     server.route({
         method: 'GET',
         path: '/html',
-        handler: (request, reply) => {
-            options.respond(reply, {renderedContent: '<p>Hello World</p>'});
-        }
+        handler: (req, reply) => renderThroughReactRouter(req.raw.req.url, reply, options.routes, options.respond)
     });
 
     next();
--- test/unit/route-test.js
+++ test/unit/route-test.js
@@ -2,39 +2,44 @@ import sinon from 'sinon';
 import {assert} from 'chai';
 import any from '@travi/any';
 import {register} from '../../src/route';
+import * as routerWrapper from '../../src/router-wrapper';
 
 suite('route', () => {
-    suite('plugin', () => {
-        test('that the plugin is defined', () => {
-            assert.deepEqual(register.attributes, {
-                name: 'html-route'
-            });
+    let sandbox;
+
+    setup(() => {
+        sandbox = sinon.sandbox.create();
+
+        sandbox.stub(routerWrapper, 'default');
+    });
+
+    teardown(() => sandbox.restore());
+
+    test('that the plugin is defined', () => {
+        assert.deepEqual(register.attributes, {
+            name: 'html-route'
         });
+    });
 
-        test('that the request for html is handled', () => {
-            const
-                next = sinon.spy(),
-                server = {route: sinon.spy()};
+    test('that the request for html is handled', () => {
+        const
+            next = sinon.spy(),
+            route = sinon.stub(),
+            respond = sinon.spy(),
+            routes = sinon.spy(),
+            reply = any.simpleObject(),
+            url = any.string();
 
-            register(server, null, next);
+        register({route}, {respond, routes}, next);
 
-            assert.calledOnce(next);
-            assert.calledWith(server.route, sinon.match({
-                method: 'GET',
-                path: '/html'
-            }));
-        });
+        assert.calledOnce(next);
+        assert.calledWith(route, sinon.match({
+            method: 'GET',
+            path: '/html'
+        }));
 
-        test('that rendered content is sent in the response', () => {
-            const
-                route = sinon.stub(),
-                respond = sinon.spy(),
-                reply = any.simpleObject();
+        route.yieldTo('handler', {raw: {req: {url}}}, reply);
 
-            register({route}, {respond}, () => undefined);
-            route.yieldTo('handler', null, reply);
-
-            assert.calledWith(respond, reply, {renderedContent: '<p>Hello World</p>'});
-        });
+        assert.calledWith(routerWrapper.default, url, reply, routes, respond);
     });
 });
