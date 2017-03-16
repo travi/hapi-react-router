cbc69d2cf4674685098be4c04f94e09d995da004
Matt Travi <programmer@travi.org>
2017-03-15 22:33:47 -0500

    refactor(sinon-as-promised): added a workaround to make sinon-as-promised work with sinon 2.0

--- scripts/mocha-setup.js
+++ scripts/mocha-setup.js
@@ -1,7 +1,16 @@
 import chai from 'chai';
 import sinon from 'sinon';
 import chaiAsPromised from 'chai-as-promised';
-import 'sinon-as-promised';
+
+sinon.behavior = require('sinon/lib/sinon/behavior');
+
+sinon.defaultConfig = {
+  injectInto: null,
+  properties: ['spy', 'stub', 'mock', 'clock', 'server', 'requests'],
+  useFakeTimers: true,
+  useFakeServer: true
+};
+require('sinon-as-promised');
 
 sinon.assert.expose(chai.assert, {prefix: ''});
 chai.use(chaiAsPromised);
