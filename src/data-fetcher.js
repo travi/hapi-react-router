16c5e4eb2d8b0a87325f4af246fbd7e2ee20804e
Matt Travi <developer@travi.org>
2019-12-09 14:29:54 -0600

    feat(get-state): passed getState to data-fetchers
    so that the fetching context isnt limited to a stale state

--- src/data-fetcher.js
+++ src/data-fetcher.js
@@ -1,9 +1,12 @@
 import {trigger} from 'redial';
 
 export default function ({renderProps, store}) {
+  const {getState} = store;
+
   return trigger('fetch', renderProps.components, {
     params: renderProps.params,
     dispatch: store.dispatch,
-    state: store.getState()
+    state: getState(),
+    getState
   });
 }
